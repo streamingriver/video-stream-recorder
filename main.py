82ba6eb35415452d78587344bc397ab099c9fe11
Mindaugas Žvirblis <mz@9g.lt>
2020-02-12 11:33:48 +0200

    fix

--- main.py
+++ main.py
@@ -54,11 +54,15 @@ app = Bottle()
 
 @app.route('/start/<start>/<end>')
 def greet(start,end):
+    _end = time.time()+(int(end)*60)
+    if int(start) != 0 {
+        _end = start + (int(end)*60)
+    }
     out = ""
     conn = sqlite3.connect("files.sqlite3")
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    rows = cursor.execute("select * from files where downloadtime >= ? and downloadtime <= ?", (start, time.time()+(int(end)*60)))
+    rows = cursor.execute("select * from files where downloadtime >= ? and downloadtime <= ?", (start, _end)
     out += """#EXTM3U
 #EXT-X-PLAYLIST-TYPE:VOD
 #EXT-X-TARGETDURATION:50
